a4e0ed1887963e9abb57f20cd2cbe9a701dac7f5
Toyota <150324709+PowerfulToyota@users.noreply.github.com>
2023-12-07 16:33:25 +0800

    三个小编程题

---
+++ 666.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int x = 0;
+//	int y = 0;
+//	int h = 0;
+//	printf("请输入x:");
+//	scanf("%d", &x);
+//	if (x > 5)
+//	{
+//		y = x + 3;
+//		printf("%d", y);
+//	}
+//	else if (0<=x<=5)
+//	{
+//		printf("0");
+//	}
+//	else if(x < 0)
+//	{
+//		h = x * 2 + 30;
+//		printf("%d", h);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int m = 0;
+//	int n = 0;
+//	float h = 0;
+//	printf("星期天数和时间：",m,n);
+//	scanf("%d%d", &m, &n);
+//	if (0 < m <= 5)
+//	{
+//		h = 20 * n - 20 * n * 0.45;
+//		printf("%f",h);
+//	}
+//	else if(5 < m <= 7)
+//	{
+//		h = 60 * 20 - 60 * 20 * 0.45;
+//		printf("%f", h);
+//	}
+//	else if (m > 7)
+//	{
+//		printf("错误");
+//	}
+//
+//}
+//
+//{
+//	int r = 0;
+//	float l = 0;
+//	float m = 0;
+//	printf("请输入圆的半径：");
+//	scanf("%d", &r);
+//	l = 2 * 3.14 * r;
+//	m = 3.14 * r * r;
+//	printf("面积：%f\n", m);
+//	printf("周长：%f\n", l);
+//	return 0;
+//}int main()
